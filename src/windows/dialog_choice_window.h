bb2d7da1104ca44d336a5e0c85556b2fcf68f534
Sean Crenshaw <seanpc9@users.noreply.github.com>
2015-09-05 20:31:07 -0400

    Update choice window with pill and colors

--- src/windows/dialog_choice_window.h
+++ src/windows/dialog_choice_window.h
@@ -2,6 +2,6 @@
 
 #include <pebble.h>
 
-#define DIALOG_CHOICE_WINDOW_MESSAGE  "Set as default?"
+#define DIALOG_CHOICE_WINDOW_MESSAGE  "Did you take your meds?"
 
 void dialog_choice_window_push();
