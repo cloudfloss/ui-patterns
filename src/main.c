cce9d5aa3c6c4ca8ddfbd3afc35cbb05a13d45ce
Sean Crenshaw <seanpc9@vt.edu>
2015-09-06 00:36:30 -0400

    Add another screen

--- src/main.c
+++ src/main.c
@@ -2,114 +2,98 @@
 
 #include "windows/dialog_choice_window.h"
 
-#define REPEAT_INTERVAL_MS 50
-
-// This is a custom defined key for saving our count field
-#define NUM_DRINKS_PKEY 1
-
-// You can define defaults for values in persistent storage
-#define NUM_DRINKS_DEFAULT 0
+#define NUM_WINDOWS 2
 
 static Window *s_main_window;
+static MenuLayer *s_menu_layer;
 
-static ActionBarLayer *s_action_bar;
-static TextLayer *s_header_layer, *s_body_layer, *s_label_layer;
-static GBitmap *s_icon_plus, *s_icon_minus;
-
-static int s_num_drinks = NUM_DRINKS_DEFAULT;
-
-static void update_text() {
-    static char s_body_text[18];
-    snprintf(s_body_text, sizeof(s_body_text), "%u Bottles", s_num_drinks);
-    text_layer_set_text(s_body_layer, s_body_text);
+static uint16_t get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *context) {
+    return NUM_WINDOWS;
 }
 
-static void increment_click_handler(ClickRecognizerRef recognizer, void *context) {
-    s_num_drinks++;
-    update_text();
-}
-
-static void decrement_click_handler(ClickRecognizerRef recognizer, void *context) {
-    if (s_num_drinks <= 0) {
-        // Keep the counter at zero
-        return;
+static void draw_row_callback(GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *context) {
+    switch(cell_index->row) {
+        case 0:
+            menu_cell_basic_draw(ctx, cell_layer, "Check Meds", NULL, NULL);
+            break;
+        case 1:
+            menu_cell_basic_draw(ctx, cell_layer, "Choice Dialog", NULL, NULL);
+            break;
+        default:
+            break;
     }
-
-    s_num_drinks--;
-    update_text();
 }
 
-static void click_config_provider(void *context) {
-    window_single_repeating_click_subscribe(BUTTON_ID_UP, REPEAT_INTERVAL_MS, increment_click_handler);
-    window_single_repeating_click_subscribe(BUTTON_ID_DOWN, REPEAT_INTERVAL_MS, decrement_click_handler);
+static int16_t get_cell_height_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
+    return CHECKBOX_WINDOW_CELL_HEIGHT;
 }
 
-static void main_window_load(Window *window) {
+static void pin_complete_callback(PIN pin, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Pin was %d %d %d", pin.digits[0], pin.digits[1], pin.digits[2]);
+    pin_window_pop((PinWindow*)context, true);
+}
+
+static void select_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
+    switch(cell_index->row) {
+        case 0:
+            dialog_choice_window_push();
+            break;
+        case 1:
+            dialog_choice_window_push();
+            break;
+        default:
+            break;
+    }
+}
+
+static void draw_header_callback(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *context) {
+    menu_cell_basic_header_draw(ctx, cell_layer, "Choose a Component");
+}
+
+static int16_t get_header_height_callback(struct MenuLayer *menu_layer, uint16_t section_index, void *context) {
+    return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+static uint16_t get_num_sections_callback(struct MenuLayer *menu_layer, void *context) {
+    return 1;
+}
+
+static void window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
 
-    s_action_bar = action_bar_layer_create();
-    action_bar_layer_add_to_window(s_action_bar, window);
-    action_bar_layer_set_click_config_provider(s_action_bar, click_config_provider);
-
-    action_bar_layer_set_icon(s_action_bar, BUTTON_ID_UP, s_icon_plus);
-    action_bar_layer_set_icon(s_action_bar, BUTTON_ID_DOWN, s_icon_minus);
-
-    int width = layer_get_frame(window_layer).size.w - ACTION_BAR_WIDTH - 3;
-
-    s_header_layer = text_layer_create(GRect(4, 0, width, 60));
-    text_layer_set_font(s_header_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-    text_layer_set_background_color(s_header_layer, GColorClear);
-    text_layer_set_text(s_header_layer, "Drink Counter");
-    layer_add_child(window_layer, text_layer_get_layer(s_header_layer));
-
-    s_body_layer = text_layer_create(GRect(4, 44, width, 60));
-    text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-    text_layer_set_background_color(s_body_layer, GColorClear);
-    layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
-
-    s_label_layer = text_layer_create(GRect(4, 44 + 28, width, 60));
-    text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-    text_layer_set_background_color(s_label_layer, GColorClear);
-    text_layer_set_text(s_label_layer, "of drinks on the wall");
-    layer_add_child(window_layer, text_layer_get_layer(s_label_layer));
-
-    update_text();
+    s_menu_layer = menu_layer_create(bounds);
+    menu_layer_set_click_config_onto_window(s_menu_layer, window);
+    menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks) {
+            .get_num_rows = (MenuLayerGetNumberOfRowsInSectionsCallback)get_num_rows_callback,
+            .draw_row = (MenuLayerDrawRowCallback)draw_row_callback,
+            .get_cell_height = (MenuLayerGetCellHeightCallback)get_cell_height_callback,
+            .select_click = (MenuLayerSelectCallback)select_callback,
+            .draw_header = (MenuLayerDrawHeaderCallback)draw_header_callback,
+            .get_header_height = (MenuLayerGetHeaderHeightCallback)get_header_height_callback,
+            .get_num_sections = (MenuLayerGetNumberOfSectionsCallback)get_num_sections_callback,
+    });
+    layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
 }
 
-static void main_window_unload(Window *window) {
-    text_layer_destroy(s_header_layer);
-    text_layer_destroy(s_body_layer);
-    text_layer_destroy(s_label_layer);
-
-    action_bar_layer_destroy(s_action_bar);
+static void window_unload(Window *window) {
+    menu_layer_destroy(s_menu_layer);
 }
 
 static void init() {
-    s_icon_plus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_PLUS);
-    s_icon_minus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_MINUS);
-
-    // Get the count from persistent storage for use if it exists, otherwise use the default
-    s_num_drinks = persist_exists(NUM_DRINKS_PKEY) ? persist_read_int(NUM_DRINKS_PKEY) : NUM_DRINKS_DEFAULT;
-
     s_main_window = window_create();
     window_set_window_handlers(s_main_window, (WindowHandlers) {
-            .load = main_window_load,
-            .unload = main_window_unload,
+            .load = window_load,
+            .unload = window_unload,
     });
     window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
-    // Save the count into persistent storage on app exit
-    persist_write_int(NUM_DRINKS_PKEY, s_num_drinks);
-
     window_destroy(s_main_window);
-
-    gbitmap_destroy(s_icon_plus);
-    gbitmap_destroy(s_icon_minus);
 }
 
-int main(void) {
+int main() {
     init();
     app_event_loop();
     deinit();
